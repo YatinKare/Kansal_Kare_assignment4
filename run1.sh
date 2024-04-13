56bda14d0b45ed8b06a42ca2944630b7b5218097
Rohan Kansal <rohankansal05@gmail.com>
2024-04-12 21:50:19 -0400

    adjusted shell files

--- run1.sh
+++ run1.sh
@@ -2,4 +2,4 @@
 javac -d bin src/sort/Sorting.java
 javac -d bin -cp bin src/sort/SortingDriver.java
 javac -d bin -cp bin src/sort/SortingDriver2.java
-java -cp bin sort.SortingDriver2 $1
+java -cp bin sort.SortingDriver $1
--- run2.sh
+++ run2.sh
@@ -2,4 +2,4 @@
 javac -d bin src/sort/Sorting.java
 javac -d bin -cp bin src/sort/SortingDriver.java
 javac -d bin -cp bin src/sort/SortingDriver.java
-java -cp bin sort.SortingDriver
+java -cp bin sort.SortingDriver2
